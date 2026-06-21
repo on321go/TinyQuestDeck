e1fe2285bf662b649d06bf83832438a15dceb3c4
Joey Rubin <josemv3@mac.com>
2026-06-21 12:33:15 -0700

    Initial Commit

---
+++ TInyQuestDeck/TInyQuestDeckApp.swift
@@ -0,0 +1,32 @@
+//
+//  TInyQuestDeckApp.swift
+//  TInyQuestDeck
+//
+//  Created by Joey Rubin on 6/21/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TInyQuestDeckApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
